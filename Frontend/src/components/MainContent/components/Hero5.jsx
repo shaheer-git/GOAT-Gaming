649a75145c38a57bad9b9bc646601cb56057b88e
shaheer-git <mshaheer1859@gmail.com>
2025-12-27 00:53:23 +0530

    # Image optimization needed (is in progress)

--- Frontend/src/components/MainContent/components/Hero5.jsx
+++ Frontend/src/components/MainContent/components/Hero5.jsx
@@ -7,35 +7,35 @@ import WallPortrait4 from '../../../assets/WallPortrait-4.jpg'
 import WallPortrait5 from '../../../assets/WallPortrait-5.jpg'
 import WallPortrait6 from '../../../assets/WallPortrait-6.jpg'
 import WallPortrait7 from '../../../assets/WallPortrait-7.jpg'
-import WallLandscape1 from '../../../assets/WallLandscape-1.jpg'
-import WallLandscape2 from '../../../assets/WallLandscape-2.jpg'
-import WallLandscape3 from '../../../assets/WallLandscape-3.jpg'
-import WallLandscape4 from '../../../assets/WallLandscape-4.jpg'
-import WallLandscape5 from '../../../assets/WallLandscape-5.jpg'
-import WallLandscape6 from '../../../assets/WallLandscape-6.jpg'
-import WallLandscape7 from '../../../assets/WallLandscape-7.jpg'
-import WallLandscape8 from '../../../assets/WallLandscape-8.jpg'
-import WallLandscape9 from '../../../assets/WallLandscape-9.jpg'
-import WallLandscape10 from '../../../assets/WallLandscape-10.jpg'
 import WallLandscape11 from '../../../assets/WallLandscape-11.jpg'
-import WallLandscape12 from '../../../assets/WallLandscape-12.jpg'
-import WallLandscape13 from '../../../assets/WallLandscape-13.jpg'
 import WallLandscape14 from '../../../assets/WallLandscape-14.jpg'
-import WallLandscape15 from '../../../assets/WallLandscape-15.jpg'
-import WallLandscape16 from '../../../assets/WallLandscape-16.jpg'
-import WallLandscape17 from '../../../assets/WallLandscape-17.jpg'
-import WallLandscape18 from '../../../assets/WallLandscape-18.jpg'
-import WallLandscape19 from '../../../assets/WallLandscape-19.jpg'
-import WallLandscape20 from '../../../assets/WallLandscape-20.jpg'
-import WallLandscape21 from '../../../assets/WallLandscape-21.jpg'
-import WallLandscape22 from '../../../assets/WallLandscape-22.jpg'
-import WallLandscape23 from '../../../assets/WallLandscape-23.jpg'
-import WallLandscape24 from '../../../assets/WallLandscape-24.jpg'
-import WallLandscape25 from '../../../assets/WallLandscape-25.jpg'
-import WallLandscape26 from '../../../assets/WallLandscape-26.jpg'
-import WallLandscape27 from '../../../assets/WallLandscape-27.jpg'
-import WallLandscape28 from '../../../assets/WallLandscape-28.jpg'
-import WallLandscape29 from '../../../assets/WallLandscape-29.jpg'
+import WallLandscape13 from '../../../assets/WallLandscape-13.jpg'
+// import WallLandscape4 from '../../../assets/WallLandscape-4.jpg'
+// import WallLandscape5 from '../../../assets/WallLandscape-5.jpg'
+// import WallLandscape6 from '../../../assets/WallLandscape-6.jpg'
+// import WallLandscape7 from '../../../assets/WallLandscape-7.jpg'
+// import WallLandscape8 from '../../../assets/WallLandscape-8.jpg'
+// import WallLandscape9 from '../../../assets/WallLandscape-9.jpg'
+// import WallLandscape10 from '../../../assets/WallLandscape-10.jpg'
+// import WallLandscape11 from '../../../assets/WallLandscape-11.jpg'
+// import WallLandscape12 from '../../../assets/WallLandscape-12.jpg'
+// import WallLandscape13 from '../../../assets/WallLandscape-13.jpg'
+// import WallLandscape14 from '../../../assets/WallLandscape-14.jpg'
+// import WallLandscape15 from '../../../assets/WallLandscape-15.jpg'
+// import WallLandscape16 from '../../../assets/WallLandscape-16.jpg'
+// import WallLandscape17 from '../../../assets/WallLandscape-17.jpg'
+// import WallLandscape18 from '../../../assets/WallLandscape-18.jpg'
+// import WallLandscape19 from '../../../assets/WallLandscape-19.jpg'
+// import WallLandscape20 from '../../../assets/WallLandscape-20.jpg'
+// import WallLandscape21 from '../../../assets/WallLandscape-21.jpg'
+// import WallLandscape22 from '../../../assets/WallLandscape-22.jpg'
+// import WallLandscape23 from '../../../assets/WallLandscape-23.jpg'
+// import WallLandscape24 from '../../../assets/WallLandscape-24.jpg'
+// import WallLandscape25 from '../../../assets/WallLandscape-25.jpg'
+// import WallLandscape26 from '../../../assets/WallLandscape-26.jpg'
+// import WallLandscape27 from '../../../assets/WallLandscape-27.jpg'
+// import WallLandscape28 from '../../../assets/WallLandscape-28.jpg'
+// import WallLandscape29 from '../../../assets/WallLandscape-29.jpg'
 
 
 
@@ -46,14 +46,14 @@ const Hero5 = () => {
     ];
 
     const landscapeImages = [
-        WallLandscape1, WallLandscape2, WallLandscape3, WallLandscape4,
-        WallLandscape5, WallLandscape6, WallLandscape7, WallLandscape8,
-        WallLandscape9, WallLandscape10, WallLandscape11, WallLandscape12,
-        WallLandscape13, WallLandscape14, WallLandscape15, WallLandscape16,
-        WallLandscape17, WallLandscape18, WallLandscape19, WallLandscape20,
-        WallLandscape21, WallLandscape22, WallLandscape23, WallLandscape24,
-        WallLandscape25, WallLandscape26, WallLandscape27, WallLandscape28,
-        WallLandscape29
+        WallLandscape11, WallLandscape13, WallLandscape14,
+        // WallLandscape5, WallLandscape6, WallLandscape7, WallLandscape8,
+        // WallLandscape9, WallLandscape10, WallLandscape11, WallLandscape12,
+        // WallLandscape13, WallLandscape14, WallLandscape15, WallLandscape16,
+        // WallLandscape17, WallLandscape18, WallLandscape19, WallLandscape20,
+        // WallLandscape21, WallLandscape22, WallLandscape23, WallLandscape24,
+        // WallLandscape25, WallLandscape26, WallLandscape27, WallLandscape28,
+        // WallLandscape29
     ];
 
     const [portraitIndex, setPortraitIndex] = useState(0);
